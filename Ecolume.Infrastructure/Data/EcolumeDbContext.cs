f052343f0cc3c347b19c0f7f693c83802dd5e6cb
Breno Abreu <brenogpa@gmail.com>
2024-10-21 13:28:40 -0300

    Initial commit

---
+++ Ecolume.Infrastructure/Data/EcolumeDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Ecolume.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecolume.Infrastructure.Data;
+
+public class EcolumeDbContext : DbContext
+{
+    public EcolumeDbContext(DbContextOptions<EcolumeDbContext> options) : base(options)
+    {
+        
+    }
+    
+    public DbSet<Product> Products { get; set; }
+}
